fed5fa59a77b70ef7d21af906cd27349385397aa
ponxpon <ponponpon816@gmail.com>
2021-05-20 11:06:26 +0000

    [Update]ヘッダー

--- app/controllers/public/customer_controller.rb
+++ app/controllers/public/customer_controller.rb
@@ -1,9 +1,11 @@
 class Public::CustomerController < ApplicationController
 
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
