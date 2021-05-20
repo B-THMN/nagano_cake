608065e18579f1d9b4ad12f35b63efd0c7e5f917
sho-ma <shomaegawa510@gmail.com>
2021-05-20 07:42:35 +0000

    [Add]商品一覧

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,19 +1,42 @@
 class Admin::ItemsController < ApplicationController
+   before_action :authenticate_admin!
+   
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def create
+    @item = Item.new(item_params)
+   if @item.save
+    redirect_to admin_items_path(@item.id)
+   else
+    render :new
+   end
   end
 
   def new
+    @item = Item.new
   end
 
   def edit
   end
 
   def update
+     @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_items_path(@item.id)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :excluding_tax_price, :is_active )
   end
 end
