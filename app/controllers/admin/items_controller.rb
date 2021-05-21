cf6daf9145b62cdebd0b237849faecd6137e6488
sho-ma <shomaegawa510@gmail.com>
2021-05-21 02:28:40 +0000

    [FIX]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -12,7 +12,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
    if @item.save
-    redirect_to admin_items_path(@item.id)
+    redirect_to admin_item_path(@item.id)
    else
     render :new
    end
@@ -31,10 +31,6 @@ class Admin::ItemsController < ApplicationController
     @item.update(item_params)
     redirect_to admin_item_path(@item.id)
 
-  end
-  private
-  def item_params
-    params.require(:item).permit(:image, :name, :introduction, :genre_id, :excluding_tax_price, :is_active )
   end
   
   private
