641e3b6a54c2c736a718c88be85b732fd69eba40
sho-ma <shomaegawa510@gmail.com>
2021-05-23 04:49:28 +0000

    [Add]cart-item

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  
+  def purchase_price(price)
+    (price * 1.1).round
+  end
+  
+  def sub_price(sub)
+    (purchase_price(sub.item.excluding_tax_price) * sub.quantity)
+  end
+  
+  def total_price(totals)
+    price = 0
+    totals.each do |total|
+      price  +=  sub_price(total)
+    end
+    return price
+  end
+  
 end
