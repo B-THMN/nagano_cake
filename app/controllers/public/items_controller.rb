3514303a4eea6ab630d0ea9ab5d76543c006e472
EC2 Default User <mmddgpcd1122@gmail.com>
2021-05-25 07:12:37 +0000

    [Update]view

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,25 +1,25 @@
 class Public::ItemsController < ApplicationController
   # before_action :authenticate_cusromer!, only: [:show]
-  
+
   def top
     @items = Item.limit(4).order(created_at: :desc)
     @genres = Genre.all
   end
-  
+
   def about
   end
- 
+
   def index
-    @items = Item.all
+    @items = Item.page(params[:page])
     @genres = Genre.all
   end
- 
+
  def show
     @items = Item.all
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
  end
-  
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :introduction, :genre_id, :excluding_tax_price, :is_active )
